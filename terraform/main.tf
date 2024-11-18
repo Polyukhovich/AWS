656ff9b9b202d2601ce3ea742009b97bb0e87606
Polyukhovich Andriy <144113422+Polyukhovich@users.noreply.github.com>
2024-11-18 11:51:25 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+backend "s3" {
+    bucket = "lab67-my-tf-state"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "lab67-my-tf-lockid" 
+
+  }
 }
 
 # Configure the AWS provider
