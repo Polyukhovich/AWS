6f9b1eef15ccf09be644dc64cc1701248576f238
Polyukhovich Andriy <144113422+Polyukhovich@users.noreply.github.com>
2024-11-22 12:28:54 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,61 +14,7 @@ backend "s3" {
 
   }
 }
-# Configure the AWS provider
-provider "aws" {
-  region     = "us-east-1"
-}
 
-variable "REPOSITORY_URI" {
-  type = string
-}
-
-resource "aws_lightsail_container_service" "flask_application" {
-  name = "flask-app"
-  power = "nano"
-  scale = 1
-
-  private_registry_access {
-    ecr_image_puller_role {
-      is_active = true
-    }
-  }
-
-
-  tags = {
-    version = "1.0.0"
-  }
-}
-
-resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
-  container {
-    container_name = "flask-application"
-
-    image = "${var.REPOSITORY_URI}:latest"
-    
-    ports = {
-      # Consistent with the port exposed by the Dockerfile and app.py
-      8080 = "HTTP"
-    }
-  }
-
-  public_endpoint {
-    container_name = "flask-application"
-    # Consistent with the port exposed by the Dockerfile and app.py
-    container_port = 8080
-
-    health_check {
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-      timeout_seconds     = 2
-      interval_seconds    = 5
-      path                = "/"
-      success_codes       = "200-499"
-    }
-  }
-
-  service_name = aws_lightsail_container_service.flask_application.name
-}
 # Configure the AWS provider
 provider "aws" {
   region     = "us-east-1"
